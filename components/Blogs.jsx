4f51004e1ff42f2e2364772dc557a2fcc831c6d2
jabed <jabedzaman2004@gmail.com>
2022-11-07 05:21:36 +0530

    fix font size

--- components/Blogs.jsx
+++ components/Blogs.jsx
@@ -15,27 +15,27 @@ export default function Blogs({ blogs }) {
                 <span className="mt-1 text-gray-500 text-sm">{blog.data.date}</span>
               </div>
               <div className="md:flex-grow">
-                <h2 className="text-2xl font-medium dark:text-gray-200 text-gray-900 title-font mb-2">
+                <h2 className="lg:text-2xl text-lg font-medium dark:text-gray-200 text-gray-900 title-font mb-2">
                   {blog.data.title}
                 </h2>
 
-                <p className="text right-0 lg:text-left text-center text-gray-700 font-light mb-2 flex flex-row space-x-3 items-center">
+                <p className="lg:text-lg text-sm right-0 lg:text-left text-center text-gray-700 font-light mb-2 flex flex-row space-x-3 items-center">
                     <SiReadthedocs className="mr-2"/>
                     {blog.data.read} mins read
                 </p>
-                <p className="leading-relaxed text-gray-700 dark:text-gray-300">{blog.data.description}</p>
+                <p className="leading-relaxed lg:text-xl text-lg text-gray-700 dark:text-gray-300">{blog.data.description}</p>
                 <Link
                   passHref
                   href={`blogs/${blog.filePath.replace(/\.mdx?$/, "")}`}
                 >
-                  <p className="text-indigo-500 cursor-pointer inline-flex items-center mt-4">
+                  <p className="text-indigo-500 lg:text-lg text-smcursor-pointer inline-flex items-center mt-4">
                     Read More
                     <svg
                       className="w-4 h-4 ml-2"
                       viewBox="0 0 24 24"
                       stroke="currentColor"
                       fill="none"
-                    >
+                  >
                       <path d="M5 12h14"></path>
                       <path d="M12 5l7 7-7 7"></path>
                     </svg>
