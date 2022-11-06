5b026b366371c93f74ecbdaf333a68f2dd07be71
jabed <jabedzaman2004@gmail.com>
2022-11-07 05:01:09 +0530

    display: components

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,49 @@
-import React from 'react'
-import Hero from '../components/Hero'
+import Hero from "../components/Hero";
+import Skills from "../components/Skills"; 
+import Projects from "../components/Projects"; 
+import Blogs from "../components/Blogs"; 
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import { BLOGS_PATH, blogFilePaths, projectFilePaths, PROJECTS_PATH } from "../utils/mdxUtils" 
 
-function Home() {
+export default function Home({ projects, blogs }) {
   return (
-    <div className=''>
-      <Hero/>
-    </div>
-  )
+      <main className="overflow-hidden">
+        <Hero />
+        <Skills /> 
+        <Projects projects={projects} /> 
+        <Blogs blogs={blogs} />
+      </main>
+  );
+}
+export function getStaticProps() {
+  const blogs = blogFilePaths.map((filePath) => {
+    const source = fs.readFileSync(path.join(BLOGS_PATH, filePath));
+    const { data } = matter(source);
+    return {
+      data,
+      filePath,
+    };
+  });
+  const projects = projectFilePaths.map((filePath) => {
+    const source = fs.readFileSync(path.join(PROJECTS_PATH, filePath));
+    const { data } = matter(source);
+    return {
+      data,
+      filePath,
+    };
+  });
+  function compare(a, b) {
+    if (a.data.index < b.data.index) {
+      return -1;
+    }
+    if (a.data.index > b.data.index) {
+      return 1;
+    }
+    return 0;
+  }
+  blogs.sort(compare);
+  return { props: { blogs, projects } };
 }
 
-export default Home
