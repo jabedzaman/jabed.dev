345d5cd94385fe7bda6e59f53e32e628796f9765
jabed <jabedzaman2004@gmail.com>
2022-11-07 07:48:55 +0530

    add: favicon

--- pages/_app.js
+++ pages/_app.js
@@ -22,6 +22,14 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
           name="keywords"
           content="jabed, developer, freelancer, full-stack-developer, web-developer, technical-blogs, computer-science"
         />
+        <meta name="author" content="Jabed" />
+        <meta name="robots" content="index, follow" />
+        <meta name="language" content="English" />
+        <meta name="revisit-after" content="1 days" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Oxygen&display=swap"
