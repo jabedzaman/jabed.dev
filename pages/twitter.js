9fd64b06c32f64783c8ddf5b1fe08b2fac03edc6
jabedzaman <jabedzaman2004@gmail.com>
2023-03-08 16:08:56 +0530

    add redirect urls

---
+++ pages/twitter.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
+
+export default function github() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("https://twitter.com/xenseee");
+  }, []);
+  return (
+    <div className="flex items-center justify-center my-10">
+      Redirecting to Twitter...
+    </div>
+  );
+}
