37e98ec4f7eadd1708d06b935d9cd9bd10a39672
jabedzaman <jabedzaman2004@gmail.com>
2023-04-30 02:03:10 +0530

    (feat) add basic contentlayer support

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,8 +1,31 @@
-// app/page.js
+import { allPosts } from "contentlayer/generated";
+import { getMDXComponent } from "next-contentlayer/hooks";
 
-export default async function Home({ params }: any) {
-  // Optionally provide a type for your frontmatter object
-  const slug = params.slug;
+export const generateStaticParams = async () =>
+  allPosts.map((post) => ({ slug: post._raw.flattenedPath }));
 
-  return <>{slug}</>;
-}
+export const generateMetadata = ({ params }) => {
+  const post = allPosts.find((post) => post._raw.flattenedPath === params.slug);
+  return { title: post.title };
+};
+
+const PostLayout = ({ params }: { params: { slug: string } }) => {
+  const post = allPosts.find((post) => post._raw.flattenedPath === params.slug);
+
+  const Content = getMDXComponent(post.body.code);
+
+  return (
+    <article className="py-8 mx-auto max-w-xl">
+      <div className="mb-8 text-center">
+        <time
+          dateTime={post.date}
+          className="mb-1 text-xs text-gray-600"
+        ></time>
+        <h1>{post.title}</h1>
+      </div>
+      <Content />
+    </article>
+  );
+};
+
+export default PostLayout;
--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,27 +1,48 @@
-import { Metadata } from "next";
+import Link from "next/link";
+import { compareDesc, format, parseISO } from "date-fns";
+import { allPosts, Post } from "contentlayer/generated";
+import { getMDXComponent } from "next-contentlayer/hooks";
 import { NextPage } from "next";
-import fs from "fs";
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Blogs",
+  title: "Blog",
 };
 
-const postsDirectory = `${process.cwd()}/content`;
-const posts = fs.readdirSync(postsDirectory);
+function PostCard(post: Post) {
+  const Content = getMDXComponent(post.body.code);
+
+  return (
+    <div className="mb-8">
+      <h2 className="text-xl">
+        <Link
+          href={post.url}
+          className="text-blue-700 hover:text-blue-900"
+          legacyBehavior
+        >
+          {post.title}
+        </Link>
+      </h2>
+      <time dateTime={post.date} className="block mb-2 text-xs text-gray-600">
+        {format(parseISO(post.date), "LLLL d, yyyy")}
+      </time>
+      <div className="text-sm">
+        <Content />
+      </div>
+    </div>
+  );
+}
 
 const page: NextPage = () => {
+  const posts = allPosts.sort((a, b) =>
+    compareDesc(parseISO(a.date), parseISO(b.date))
+  );
+
   return (
-    <div>
-      <h1 className="text-2xl font-semibold">Blogs</h1>
-      <ul>
-        {posts.map((post) => {
-          return (
-            <li key={post}>
-              <a href={`/posts/${post}`}>{post}</a>
-            </li>
-          );
-        })}
-      </ul>
+    <div className="py-8 mx-auto max-w-xl">
+      {posts.map((post) => (
+        <PostCard key={post._raw.flattenedPath} {...post} />
+      ))}
     </div>
   );
 };
