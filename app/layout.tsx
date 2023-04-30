7a757ef02457b5c1495359b42e34aaa96395775b
jabedzaman <jabedzaman2004@gmail.com>
2023-04-30 05:58:23 +0530

    (fix) no index in robots.txt

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,17 @@ export const metadata: Metadata = {
   twitter: {
     title: siteConfig.title,
   },
-  robots: "index, follow",
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
   icons: {
     shortcut: "/favicon.ico",
   },
