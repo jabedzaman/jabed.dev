4c34fa8edc55cade7ea1b388e03e753221d1b26e
jabedzaman <jabedzaman2004@gmail.com>
2023-05-08 02:20:36 +0530

    fix

--- utils/index.ts
+++ utils/index.ts
@@ -1,5 +1,4 @@
-export const URL: String =
-  process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000";
+export const URL: String = process.env.NEXT_PUBLIC_SITE_URL as string;
 
 export const isDev: boolean =
   process.env.NODE_ENV === "development" ? true : false;
