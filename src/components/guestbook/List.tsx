b93aa884afcfb2d61cdebb26f2177d34c9a26e7f
jabed zaman <hi@jabed.dev>
2024-04-18 19:02:18 +0530

    feat: add guestbook input

--- src/components/guestbook/List.tsx
+++ src/components/guestbook/List.tsx
@@ -8,15 +8,25 @@ export const List: React.FC = React.memo(async () => {
     query(collection(db, "signatures"), orderBy("created_at", "desc"))
   );
   const docs = signatures.docs.map((doc) => {
-    return doc.data();
-  }) as { id: string; message: string; name: string; created_at: Date }[];
-  console.log(docs);
+    return {
+      id: doc.id,
+      name: doc.data().name,
+      created_at: doc.data().created_at.toDate().toISOString(),
+      message: doc.data().message,
+    };
+  }) as { id: string; message: string; name: string; created_at: string }[];
   return (
     <div className="my-5">
       {docs.map((doc) => {
         return (
-          <div key={doc.id} className="my-2">
-            <p className="text-sm text-[#666666]">{doc.name}</p>
+          <div key={doc.id} className="my-6">
+            <p className="text-sm text-[#666666]">
+              <span>{doc.name}</span>
+              <span> </span>
+              <span className="text-xs">
+                ({moment(doc.created_at).fromNow()})
+              </span>
+            </p>
             <p>{doc.message}</p>
           </div>
         );
