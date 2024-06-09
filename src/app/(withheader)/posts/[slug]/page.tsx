86f33709d9897dbd99f2cb10828cd39ecdea1231
jabedzaman <hi@jabed.dev>
2024-06-09 09:59:57 +0530

    feat: new system design guide

--- src/app/(withheader)/posts/[slug]/page.tsx
+++ src/app/(withheader)/posts/[slug]/page.tsx
@@ -30,6 +30,7 @@ export default async function Page({ params }: { params: { slug: string } }) {
     return notFound();
   }
   const { content, frontmatter } = await getMdxContent(params.slug);
+  const tags = frontmatter.keywords.split(",");
   return (
     <main className="my-10">
       <h1 className={`${bricolage.className} text-4xl`}>{frontmatter.title}</h1>
@@ -37,6 +38,16 @@ export default async function Page({ params }: { params: { slug: string } }) {
         {moment(frontmatter.date).format("MMMM D, YYYY")} (
         {moment(frontmatter.date).fromNow()})
       </p>
+      <div className="flex flex-row flex-wrap gap-2 my-2">
+        {tags.map((tech, index) => (
+          <span
+            key={index}
+            className="text-xs bg-[#1a1a1a] text-[#666666] px-2 py-1 rounded-md hover:text-[#8a8a8a] duration-200 ease-in-out cursor-pointer"
+          >
+            # {tech}
+          </span>
+        ))}
+      </div>
       <Content content={content} />
     </main>
   );
