b3b6af0956b86d6c8776d378e75f2a3500d82885
jabedzaman <hi@jabed.dev>
2024-06-09 10:05:48 +0530

    feat: added header

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -2,6 +2,19 @@ import * as React from "react";
 import { Time } from "./Time";
 import { LastVisit } from "./LastVisit";
 import { Back } from "./Back";
+import Link from "next/link";
+import { sora } from "~/libs";
+
+type Links = {
+  label: string;
+  href: string;
+};
+
+const links: Links[] = [
+  { label: "posts", href: "/posts" },
+  { label: "work", href: "/work" },
+  { label: "stack", href: "/stack" },
+];
 
 export const Header: React.FC = React.memo(() => {
   return (
@@ -10,7 +23,20 @@ export const Header: React.FC = React.memo(() => {
         <Time />
         {process.env.NODE_ENV !== "development" && <LastVisit />}
       </div>
-      <Back />
+      <div className="flex justify-between items-center mt-4">
+        <Back />
+        <nav className="flex justify-center items-center gap-2">
+          {links.map((link) => (
+            <Link
+              key={link.label}
+              href={link.href}
+              className="text-sm text-[#616161] hover:text-[#abaaaa]"
+            >
+              <span className={sora.className}>{link.label}</span>
+            </Link>
+          ))}
+        </nav>
+      </div>
     </header>
   );
 });
