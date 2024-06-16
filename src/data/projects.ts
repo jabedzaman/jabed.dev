61c6de164fb31c72c68b49af023eb663b152fc09
jabedzaman <hi@jabed.dev>
2024-06-16 22:16:46 +0530

    feat: added new project

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -66,6 +66,31 @@ export const projects: ProjectProps[] = [
     status: "on-going",
   },
   {
+    displayName: "ransom",
+    description:
+      "Complete SAAS Starter Kit with Authentication, Authorization, Internationalization, payment gateway, and more. We all have that one side project we're eager to launch. But setting up authentication, authorization, payment gateways, and other crucial features can be a real time sink. Ransom comes packed with all these, helping you get your project off the ground in no time.",
+    link: "https://ransom.jabed.dev/",
+    preview: "https://ransom.jabed.dev/",
+    repo: {
+      owner: "jabedzaman",
+      name: "ransom",
+      isPrivate: false,
+    },
+    techStack: [
+      "react",
+      "nextjs",
+      "@redux/toolkit",
+      "rtk-query",
+      "shadcn-ui",
+      "nestjs",
+      "nodejs",
+      "postgresql",
+      "turborepo",
+      "vercel"
+    ],
+    status: "on-going",
+  },
+  {
     displayName: "spotiloader (unmaintained)",
     description:
       "Download your Spotify playlists and songs with ease. Spotiloader, react native app, allows you to download your Spotify, Apple Music, and YouTube playlists and songs with ease.",
