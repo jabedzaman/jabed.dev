ce93ea72ca2447a5d56ab5fe203f1176d1d151e1
jabedzaman <hi@jabed.dev>
2024-12-02 01:23:51 +0530

    ci: auto gen image and push to ghcr

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   pageExtensions: ["tsx", "ts", "js", "mjs", "mdx"],
   redirects: async () => {
     return [
