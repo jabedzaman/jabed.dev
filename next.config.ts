2cd57067912c34b5aa54fcec636b186b6a9c2f5e
jabedzaman <hi@jabed.dev>
2025-12-05 23:08:23 +0530

    feat: added redirects

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,30 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [{ hostname: "i.scdn.co" }],
   },
+  redirects: async () => {
+    return [
+      {
+        source: "/github",
+        destination: "https://github.com/jabedzaman",
+        permanent: true,
+      },
+      {
+        source: "/git",
+        destination: "https://github.com/jabedzaman",
+        permanent: true,
+      },
+      {
+        source: "/resume",
+        destination: "https://cdn.jabed.dev/resume.pdf",
+        permanent: true,
+      },
+      {
+        source: "/cv",
+        destination: "https://cdn.jabed.dev/resume.pdf",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
